b714661f090412b7635415850d884d3271ce5d4d
yangzq <yangzq@infore.com>
2021-06-22 11:10:38 +0800

    update: demo

--- src/components/Demo/index.tsx
+++ src/components/Demo/index.tsx
@@ -26,11 +26,12 @@ function App() {
     1,
     1000
   );
+  // const camera = new PerspectiveCamera(45, $w / $h, 0.1, 1000);
   // 相机：位置属性
   // https://www.cnblogs.com/v-weiwang/p/6072235.html
-  // camera.position.x = 100;
-  // camera.position.y = 100;
-  camera.position.z = 100;
+  camera.position.x = 200;
+  camera.position.y = 200;
+  camera.position.z = 200;
   camera.lookAt(scene.position); // 0,0,0
   // camera.lookAt(30, 30, 30);
 
@@ -40,16 +41,29 @@ function App() {
   renderer.setSize($w, $h);
   // 坐标系
   const axes = new AxesHelper(300);
+
+  const tr1 = triangle("#c04851");
+  const tr2 = triangle("#eea2a4");
+
+  // tr1.position.x = 6;
+  // tr1.position.y = 6;
+  tr1.position.z = -40;
+
+  // tr2.position.x = -10;
+  // tr2.position.y = -10;
+  tr2.position.z = 10;
+
   // 添加
   scene.add(axes);
-  scene.add(triangle);
+  scene.add(tr1);
+  scene.add(tr2);
 
   // animate loop
   const animate = function () {
     // 类似于定时器
     requestAnimationFrame(animate);
 
-    triangle.rotation.x += 0.02;
+    // tr1.rotation.x += 0.02;
 
     renderer.render(scene, camera);
   };
--- src/components/Demo/triangle.ts
+++ src/components/Demo/triangle.ts
@@ -6,19 +6,18 @@ import {
   DoubleSide,
 } from "three";
 
-const x = 0,
-  y = 100;
-const shape = new Shape();
-shape.moveTo(x, y);
-shape.lineTo(x + 50, y + -100);
-shape.lineTo(x - 50, y + -100);
-shape.lineTo(x, y);
-const geometry = new ShapeGeometry(shape);
-const material = new MeshBasicMaterial({ color: "#951c48", side: DoubleSide });
-const triangle = new Mesh(geometry, material);
-
-triangle.position.x = 6;
-triangle.position.y = 6;
-// triangle.position.z = 3;
+const triangle = (color = "black", x = 0, y = 0, c = 100, h = 100) => {
+  const shape = new Shape();
+  shape.moveTo(x, y);
+  shape.lineTo(x + c / 2, y - h);
+  shape.lineTo(x - c / 2, y - h);
+  shape.lineTo(x, y);
+  const geometry = new ShapeGeometry(shape);
+  const material = new MeshBasicMaterial({
+    color,
+    side: DoubleSide,
+  });
+  return new Mesh(geometry, material);
+};
 
 export default triangle;
