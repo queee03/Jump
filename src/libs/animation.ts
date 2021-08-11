b8b877c60c3e2435f8b141bbdcbc9335a2736646
yangzq <yangzq@infore.com>
2021-08-11 18:17:55 +0800

    update: animation

---
+++ src/libs/animation.ts
@@ -0,0 +1,18 @@
+/* 
+    @description animation library
+    @detail requestAnimationFrame
+    1. duration
+    2. from
+    3. to
+    4. type
+*/
+
+/// <reference path="animation.d.ts"/>
+
+const customAnimation: customAnimationType = {};
+
+// customAnimation.to = function (duration, from, to, type) {};
+
+// function TweenAnimation(from, to, duration) {}
+
+export default customAnimation;
